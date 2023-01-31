ea3212eea75350a323efaecb7621b64bae872f6a
Vladimir Smirnov <v.smirnov@movavi.com>
2023-01-31 03:56:25 +0300

    Move thirdparty and cmake code from the FreeHeroes.

---
+++ mernel_thirdparty.cmake
@@ -0,0 +1,45 @@
+
+# Zlib
+
+if (MSVC)
+    AddTarget(TYPE static NAME zlib 
+        SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR}/3rdparty/zlib
+        EXPORT_INCLUDES
+        SKIP_STATIC_CHECK
+        COMPILE_OPTIONS
+            /wd4996 
+            /wd4131 # 'xxxx': uses old-style declarator
+        INTERFACE_COMPILE_DEFINITIONS USE_ZLIB
+    )
+else()
+    AddTarget(TYPE interface NAME zlib 
+        SOURCE_DIR .
+        LINK_LIBRARIES -lz
+        INTERFACE_COMPILE_DEFINITIONS USE_ZLIB
+    )
+endif()
+
+# Json
+AddTarget(TYPE interface NAME rapidjson 
+    SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR}/3rdparty)
+
+# frozen 
+AddTarget(TYPE interface NAME frozen 
+    SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR}/3rdparty/frozen/include)
+
+
+# Google Test
+set(BUILD_GMOCK OFF)
+set(INSTALL_GTEST OFF)
+set(GOOGLETEST_VERSION 1.10.0)
+set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
+
+add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/3rdparty/googletest/googletest)
+
+# 7-zip
+AddTarget(TYPE static NAME 7zip_static
+    SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR}/3rdparty/7zip
+    EXPORT_INCLUDES
+    SKIP_STATIC_CHECK
+    STATIC_RUNTIME
+)
