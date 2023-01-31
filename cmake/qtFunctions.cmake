f62ba0b22c88456720a3231248ef55ec2903113d
Vladimir Smirnov <v.smirnov@movavi.com>
2023-01-31 05:11:09 +0300

    Add couple more qt cmake targets.

--- cmake/qtFunctions.cmake
+++ cmake/qtFunctions.cmake
@@ -99,3 +99,55 @@ function(AddQrcOutput rccName qrcName)
                        MAIN_DEPENDENCY ${qrcName}
                        DEPENDS ${fileListAbsConfig} VERBATIM)
 endfunction()
+
+function(AddQrcTarget qrcPath)
+	get_filename_component(filename ${qrcPath} NAME_WE)
+	get_filename_component(qrcParent ${qrcPath} PATH)
+	set(rccPath ${CMAKE_BINARY_DIR}/assetsCompiled/${filename}.rcc)
+
+	set(fileListAbsConfig)
+	file(READ "${qrcPath}" fileContents)
+	string(REGEX MATCHALL "<file[^<]+" qrcFiles "${fileContents}")
+	string(REGEX REPLACE "<file[^>]*>" "" qrcFiles "${qrcFiles}")
+	set(_RC_DEPENDS)
+	foreach(name ${qrcFiles})
+		list(APPEND fileListAbsConfig "${qrcParent}/${name}")
+	endforeach()
+	add_custom_command(OUTPUT ${rccPath}
+                       COMMAND ${QT_CMAKE_EXPORT_NAMESPACE}::rcc
+                       ARGS --no-compress --binary -o ${rccPath} ${qrcPath}
+                       MAIN_DEPENDENCY ${qrcPath}
+                       DEPENDS ${fileListAbsConfig} VERBATIM)
+endfunction()
+
+
+function(GenerateQrcWithTranslations resourceFolder translationsRoot)
+    set(srcDir ${translationsRoot})
+    set(destDir ${CMAKE_BINARY_DIR}/assetsCompiled/${resourceFolder})
+    set(qrcName ${CMAKE_BINARY_DIR}/assetsCompiled/${resourceFolder}.qrc)
+    set(rccName ${CMAKE_BINARY_DIR}/assetsCompiled/${resourceFolder}.rcc)
+    file(MAKE_DIRECTORY "${destDir}")
+    set(masks ${srcDir}/*.ts)
+    file(GLOB_RECURSE fileListAbsSrc   ${masks})
+
+    set(QRC_PREFIX ${resourceFolder}) # name is important for .in file
+    set(QRC_FILES)                    # name is important for .in file
+
+    set(fileListAbsConfig)
+    foreach(tsFileAbs ${fileListAbsSrc})
+        string(REPLACE "${srcDir}/" "" relFile "${tsFileAbs}")
+        string(REPLACE ".ts" ".qm" relFile "${relFile}")
+        set(qmFileAbs "${destDir}/${relFile}")
+        list(APPEND fileListAbsConfig ${qmFileAbs})
+
+        #configure_file(${absFile} ${destFileAbs} COPYONLY)
+        add_custom_command(OUTPUT ${qmFileAbs}
+                           COMMAND ${QT_CMAKE_EXPORT_NAMESPACE}::lrelease
+                           ARGS ${tsFileAbs} -qm ${qmFileAbs}
+                           MAIN_DEPENDENCY ${tsFileAbs} VERBATIM)
+
+        string(REPLACE "${resourceFolder}/" "" file "${relFile}")
+        set(QRC_FILES "${QRC_FILES}<file alias=\"${file}\">${resourceFolder}/${relFile}</file>\n")
+    endforeach()
+    AddQrcOutput(${rccName} ${qrcName} ${fileListAbsConfig})
+endfunction()
